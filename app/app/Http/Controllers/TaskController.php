79bdacda1320c258bd111ce078b653da1e347d9f
Regulus <Regulus@Regulus.local>
2023-01-08 17:49:34 +0900

    Changed the Taskcontroller.php correctly

--- app/app/Http/Controllers/TaskController.php
+++ app/app/Http/Controllers/TaskController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
+// Javaと一緒。TaskControllerにControllerクラスを継承している。
 class TaskController extends Controller
 {
     public function index()
     {
-        return "Hello world";
+        //Folderモデルのallメソッドで全てのデータを取得
+        $folders = Folder::all();
+
+        //view関数でテンプレートに取得データを渡す
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 }
