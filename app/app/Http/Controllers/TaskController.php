1ee9c0738c3cd0b7577c1064fbd6c184bebedb3d
Regulus <Regulus@Regulus.local>
2023-01-12 08:07:13 +0900

    Add a process to get the task in the TaskController

--- app/app/Http/Controllers/TaskController.php
+++ app/app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 // Javaと一緒。TaskControllerはControllerクラスを継承
@@ -10,13 +11,20 @@ class TaskController extends Controller
 {
     public function index(int $id)
     {
-        //Folderモデルのallメソッドで全てのデータを取得
+        //Folderモデルのallメソッドで全てのフォルダを取得
         $folders = Folder::all();
+        
+        //選ばれたフォルダを取得
+        $current_folder = Folder::find($id);
+
+        //選ばれたフォルダに紐づくタスクを取得
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
 
         //view関数でテンプレートに取得データを渡す
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'task' => $tasks,
         ]);
     }
 }
