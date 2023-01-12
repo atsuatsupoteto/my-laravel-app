42f31f21f9669288ef049bb813a89feada75dc27
Regulus <Regulus@Regulus.local>
2023-01-12 23:02:34 +0900

    Format task status to Japanese instead of numeric

--- app/app/Models/Task.php
+++ app/app/Models/Task.php
@@ -7,5 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
+    /**
+     * 状態定義
+     */
+    const STATUS = [
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
+    ];
+
+    /**
+     * 状態のラベル
+     * @return string
+     */
+    public function getStatusLabelAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければnull
+        if(!isset(self::STATUS[$status])){
+            return '';
+        }
+        return self::STATUS[$status]['label'];
+    }
 }
