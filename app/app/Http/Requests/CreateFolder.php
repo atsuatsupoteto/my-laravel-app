f9f18a8d5d19ea0508fce63de25d3b49151804ae
Regulus <Regulus@Regulus.local>
2023-01-13 13:30:34 +0900

    Add required check for folder create

---
+++ app/app/Http/Requests/CreateFolder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateFolder extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        //この機能は使用しないため、trueにしておく
+        //return false;
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    //rulesメソッドでチェックするルールを定義する
+    public function rules()
+    {
+        return [
+            //requiredはLaravelがデフォルトで提供している必須チェック
+            'title' => 'required',
+        ];
+    }
+    public function attributes()
+    {
+        return [
+            'title' => 'フォルダ名',
+        ];
+    }
+}
