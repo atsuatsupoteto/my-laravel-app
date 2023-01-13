c1e8e1a793d5d07f03365b1539d0a7c1e20a522d
Regulus <Regulus@Regulus.local>
2023-01-13 12:35:39 +0900

    Add folder create screen

---
+++ app/app/Http/Controllers/FolderController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Folder;
+use Illuminate\Http\Request;
+
+class FolderController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }
+
+    // 引数にインポートしたRequestクラスを受け入れる
+    public function create(Request $request)
+    {
+        //フォルダモデルのインスタンスを作成
+        $folder = new Folder();
+
+        //タイトルに入力値を代入
+        $folder->title = $request->title;
+
+        //インスタンスの状態をデータベースに書き込む
+        $folder->save();
+        
+        return redirect()->route('tasks.index',[
+            'id' => $folder->id,
+        ]);
+
+    }
+}
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,3 +1,6 @@
 <?php
 
 Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
+
+Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
