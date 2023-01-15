277e3f504b9fbbfe886f2716f152707682146240
Regulus <Regulus@Regulus.local>
2023-01-16 08:37:58 +0900

    Add Task editing function

--- app/app/Http/Controllers/TaskController.php
+++ app/app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use App\Models\Folder;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 
 // Javaと一緒。TaskControllerはControllerクラスを継承
 class TaskController extends Controller
@@ -50,6 +51,35 @@ class TaskController extends Controller
 
         return redirect()->route('tasks.index', [
             'id' => $current_folder->id,
+        ]);
+    }
+    
+    /**
+     * GET /folders/{id}/tasks/{task_id}/edit
+     */
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request){
+        
+        //リクエストされたidでタスクデータの取得
+        $task = Task::find($task_id);
+
+        //編集対象のタスクデータに入力値を保存
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        //編集対象の含まれるタスク一覧へリダイレクト
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
         ]);
     }
 }
