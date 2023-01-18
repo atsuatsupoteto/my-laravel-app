7ebf8c35d838249b4c9be81b3a1ff71261e32827
Regulus <Regulus@Regulus.local>
2023-01-18 21:43:26 +0900

    Add_Show 403 page when accessing non-existent content

--- app/routes/web.php
+++ app/routes/web.php
@@ -31,18 +31,22 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/folders/{folder}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
-
+    Route::middleware('can:view,folder')->group(function () {
+        Route::get('/folders/{folder}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
+        
+        Route::get('/folders/{folder}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
+        Route::post('/folders/{folder}/tasks/create', 'App\Http\Controllers\TaskController@create');
+        
+        Route::get('/folders/{folder}/tasks/{task}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
+        Route::post('/folders/{folder}/tasks/{task}/edit', 'App\Http\Controllers\TaskController@edit');
+    });
     Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
     Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
 
-    Route::get('/folders/{folder}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
-    Route::post('/folders/{folder}/tasks/create', 'App\Http\Controllers\TaskController@create');
-    
-    Route::get('/folders/{folder}/tasks/{task}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
-    Route::post('/folders/{folder}/tasks/{task}/edit', 'App\Http\Controllers\TaskController@edit');
+
     
     Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 });
+
 
 require __DIR__.'/auth.php';
