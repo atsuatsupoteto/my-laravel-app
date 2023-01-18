192ef2ef1b86282143f5a8d3088faf8d455a789f
Regulus <Regulus@Regulus.local>
2023-01-18 13:25:20 +0900

    Add implementation so that you can not access the creation / viewing page of folders and tasks without login

--- app/routes/web.php
+++ app/routes/web.php
@@ -14,20 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
-
-Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
-Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
-
-Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
-
-Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
-
-Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
-
-
 //Route::get('/', function () {
 //    return view('welcome');
 //});
@@ -35,6 +21,7 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 //Route::get('/dashboard', function () {
 //    return view('dashboard');
 //})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -43,6 +30,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
+    Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
+    Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
+    Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
+    Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
+    Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
+    Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
+    Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 });
 
 require __DIR__.'/auth.php';
